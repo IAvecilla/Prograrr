bf25752bd8f58d675c94afca566e5051805ebc19
IAvecilla <iavecilla@fi.uba.ar>
2026-01-15 13:44:40 -0300

    Remove built dependencies

--- build/dev/erlang/prograrr/include/models@request_JellyseerrRequest.hrl
+++
@@ -1,8 +0,0 @@
--record(jellyseerr_request, {
-    id :: integer(),
-    media_type :: binary(),
-    status :: integer(),
-    requested_by :: gleam@option:option(binary()),
-    created_at :: gleam@option:option(binary()),
-    media :: gleam@option:option(models@request:jellyseerr_media())
-}).
